1af87a02246a7d400c1083c63c7dcff61b7d9ff8
Renan Martinez <remam_martinez89@hotmail.com>
2022-09-06 17:41:17 -0300

    create type category and data

--- src/Data/Items.ts
+++ src/Data/Items.ts
@@ -1,4 +1,6 @@
-export const items = [
+import { Item } from "../Types/Item";
+
+export const items:Item[] = [
   { date: new Date(2021, 9, 15), category: "food", title: "Lanche", value: 28.50},
   { date: new Date(2021, 9, 18), category: "food", title: "Pizza", value: 32},
   { date: new Date(2021, 9, 20), category: "rent", title: "Aluguel", value: 1400},
